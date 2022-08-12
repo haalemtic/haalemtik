9d514a95fa82bdebd38a06c84179cfe2d4236fbf
haalemtic <haalemtic@gmail.com>
2022-08-12 19:03:16 +0000

    final of today

--- assets/js/formation.js
+++ assets/js/formation.js
@@ -8,8 +8,14 @@
 
 function checkSubmenuState() {
 
- document.getElementById("formation_submenu").classList.toggle("show");
- document.getElementById("chevron").classList.toggle("bi-caret-up-fill");
+    document.getElementById("formation_submenu").classList.toggle("show");
+    document.getElementById("chevron").classList.toggle("bi-caret-up-fill");
+    if (window.innerWidth < 990) {
+        document.getElementById("mobile_submenu1").classList.toggle("show_mobile_submenu");
+        document.getElementById("mobile_submenu2").classList.toggle("show_mobile_submenu");
+    }
+ 
+
 }
 
 
