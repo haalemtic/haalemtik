8a4a555bbb0201019e402a41118caa3735078f29
haalemtic <haalemtic@gmail.com>
2022-08-12 10:50:43 +0000

    formation

---
+++ assets/js/formation.js
@@ -0,0 +1,8 @@
+const link = document.getElementsByClassName('.formation_link');
+link.addEventListener('click', e => {
+
+    document.getElementsByClassName(".formation_submenu").style.display="none";
+});
+
+
+document.getElementsByClassName("body").style.color="blue";
