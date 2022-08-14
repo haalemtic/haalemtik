fb8478bb9dbb047bc56333ab6aa425fa4556b86c
haalemtic <haalemtic@gmail.com>
2022-08-14 16:35:09 +0000

    inscription page

--- assets/js/formation.js
+++ assets/js/formation.js
@@ -3,16 +3,19 @@
 
 
 
+function showPasswordField() {
+    document.getElementById("password_field").style.display = "block";
+    document.getElementById("password_text").style.display = "block";
+}
 
 
+function bodyAction(scrollState) {
 
-function bodyAction(scrollState){
+    document.getElementById("searchBar").style.display = "none";
+    document.getElementById("search_btn").style.display = "block";
+    document.getElementById("formation_link").style.display = "block";
+    document.getElementById("startut_link").style.display = "block";
 
-        document.getElementById("searchBar").style.display="none";
-        document.getElementById("search_btn").style.display="block";
-        document.getElementById("formation_link").style.display="block";
-        document.getElementById("startut_link").style.display="block";
-      
 }
 
 
@@ -25,19 +28,19 @@ function checkSubmenuState() {
         document.getElementById("mobile_submenu1").classList.toggle("show_mobile_submenu");
         document.getElementById("mobile_submenu2").classList.toggle("show_mobile_submenu");
     }
- 
+
 
 }
 
 
-function displaySearchBar(){
-   if(window.innerWidth>990){
+function displaySearchBar() {
+    if (window.innerWidth > 990) {
 
-    document.getElementById("search_btn").classList.toggle("hide_search_btn");
-    document.getElementById("formation_link").classList.toggle("hide_menu");
-    document.getElementById("startut_link").classList.toggle("hide_menu");
-    document.getElementById("searchBar").classList.toggle("display_search_bar");
-   }
+        document.getElementById("search_btn").classList.toggle("hide_search_btn");
+        document.getElementById("formation_link").classList.toggle("hide_menu");
+        document.getElementById("startut_link").classList.toggle("hide_menu");
+        document.getElementById("searchBar").classList.toggle("display_search_bar");
+    }
 }
 
 
